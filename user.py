d781e027260490846eb1008e6484391cc897a1e1
JasonHarrer <jazzyeagle79@yahoo.com>
2021-06-16 06:53:25 -0500

    Updated to use chaining as part of next assignment

--- user.py
+++ user.py
@@ -8,18 +8,22 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(f'User: {self.name}: ${self.account_balance}')
         if self.account_balance < 0:
             print('*** THIS ACCOUNT IS OVERDRAFTED ***')
+        return self
 
     def transfer_money(self, other_user, amount):
        self.make_withdrawal(amount)
        other_user.make_deposit(amount)
+       return self
 
 
 # Test section
@@ -28,30 +32,17 @@ alice  = User("Alice I'dunno",   "alice@somecompany.com")
 mickey = User("Mickey Mickey",   "youre@sofine.org")
 
 #Test 1: Have the first user make 3 deposits and 1 withdrawal and then display their balance
-jeremy.make_deposit(200)
-jeremy.make_deposit(300)
-jeremy.make_deposit(500)
-jeremy.make_withdrawal(800)
-jeremy.display_user_balance()
+jeremy.make_deposit(200).make_deposit(300).make_deposit(500).make_withdrawal(800).display_user_balance()
 
 
 #Test 2: Have the second user make 2 deposits and 2 withdrawals and then display their balance
-alice.make_deposit(500)
-alice.make_deposit(400)
-alice.make_withdrawal(650)
-alice.make_withdrawal(100)
-alice.display_user_balance()
+alice.make_deposit(500).make_deposit(400).make_withdrawal(650).make_withdrawal(100).display_user_balance()
 
 
 #Test 3: Have the third user make 1 deposit and 3 withdrawals and then display their alance
-mickey.make_deposit(500)
-mickey.make_withdrawal(200)
-mickey.make_withdrawal(100)
-mickey.make_withdrawal(300)
-mickey.display_user_balance()
+mickey.make_deposit(500).make_withdrawal(200).make_withdrawal(100).make_withdrawal(300).display_user_balance()
 
 
 #Test 4: Have the first user transfer money to the third user and then print both users' balances'
-jeremy.transfer_money(mickey, 100)
-jeremy.display_user_balance()
+jeremy.transfer_money(mickey, 100).display_user_balance()
 mickey.display_user_balance()
